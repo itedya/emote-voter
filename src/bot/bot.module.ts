291243e65ea637ef2f2de239109fd79a638b50d7
itedya <kontakt@itedya.com>
2022-01-21 00:36:05 +0100

    Added discord client bootstrap function

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import {PrismaModule} from "../prisma/prisma.module";
+import {ConfigModule} from "../config/config.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule],
   providers: [BotService]
 })
 export class BotModule {}
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
+import {Client, Intents} from "discord.js";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class BotService {
-    constructor(private prismaService: PrismaService) {
+    constructor(private prismaService: PrismaService,
+                private configService: ConfigService) {
+    }
+
+    private logger = new Logger("BotService");
+    private client: Client;
+
+    async onApplicationBootstrap() {
+        this.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+        this.client.once('ready', () => {
+            this.logger.log("Discord client ready!");
+        });
+
+        await this.client.login(this.configService.get<string>("discord.token"));
     }
 }
