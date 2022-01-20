f17789e310b8dae3fddeb76c22c7f3d39758f6d0
itedya <kontakt@itedya.com>
2022-01-20 23:27:00 +0100

    Fresh start - part 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,6 @@
-import { Module } from "@nestjs/common";
-import { AuthModule } from "./auth/auth.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "./users/users.module";
-import { SettingsModule } from "./settings/settings.module";
-import { BotModule } from "@/bot/bot.module";
-import { EmoteChangesModule } from "./emote-changes/emote-changes.module";
-import { ScheduleModule } from "@nestjs/schedule";
-import { BttvModule } from "@/bttv/bttv.module";
-import { QueueModule } from "./queue/queue.module";
-import { BullModule } from "@nestjs/bull";
-import { CacheModule } from './cache/cache.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    ScheduleModule.forRoot(),
-    BullModule.forRoot({
-      redis: {
-        host: "localhost",
-        port: 6379
-      }
-    }),
-    AuthModule,
-    UsersModule,
-    SettingsModule,
-    EmoteChangesModule,
-    BttvModule,
-    BotModule,
-    QueueModule,
-    CacheModule
-  ],
-  controllers: [],
-  providers: []
+  imports: [],
 })
-export class AppModule {
-}
+export class AppModule {}
