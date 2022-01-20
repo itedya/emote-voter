9b598e2502c014de705f0c7686a8128b282798d6
itedya <kontakt@itedya.com>
2022-01-21 00:11:35 +0100

    Added bot service

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BotService } from './bot.service';
+import {PrismaModule} from "../prisma/prisma.module";
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [BotService]
+})
 export class BotModule {}
---
+++ src/bot/bot.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+
+@Injectable()
+export class BotService {
+    constructor(private prismaService: PrismaService) {
+    }
+}
