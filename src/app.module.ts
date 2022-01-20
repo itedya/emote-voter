412d5bd09931dc9182141cdd26c1d9e421a135e2
itedya <kontakt@itedya.com>
2022-01-21 00:29:19 +0100

    Renamed config extension and expanded config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BotModule } from './bot/bot.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [BotModule, PrismaModule],
+  imports: [BotModule, PrismaModule, ConfigModule],
 })
 export class AppModule {}
