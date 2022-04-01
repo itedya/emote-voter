69b1cdd561a67d0f38e412870f7e260009ad6e14
itedya <kontakt@itedya.com>
2022-04-01 07:50:51 +0200

    Remove bot module, updated dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { BotModule } from './bot/bot.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [BotModule, PrismaModule, ConfigModule],
+  imports: [PrismaModule, ConfigModule],
 })
 export class AppModule {}
--- src/interfaces/bot/command-handler.ts
+++
@@ -1,5 +0,0 @@
-import {CommandInteraction} from "discord.js";
-
-export interface CommandHandler {
-    executeCommand: (interaction: CommandInteraction) => void
-}
