ee37e780ea4d81f3e59eec5936db43178715b3d9
itedya <kontakt@itedya.com>
2022-04-03 15:08:32 +0200

    Added users module dependencies and export of users service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
-import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    controllers: [UsersController],
-    providers: [UsersService]
+  imports: [PrismaModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
 })
-export class UsersModule {
-
-}
+export class UsersModule {}
