0823b6856075cafe15560cb1cf0536fc38214ff4
itedya <kontakt@itedya.com>
2022-04-03 15:09:01 +0200

    Created create method in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from '../globals/dtos/users/create-user.dto';
+import { UserDto } from '../globals/dtos/users/user.dto';
 
 @Injectable()
 export class UsersService {
-    
+  constructor(private prismaService: PrismaService) {}
+
+  async create(dto: CreateUserDto): Promise<UserDto> {
+    const result = await this.prismaService.user.create({ data: dto });
+
+    if (result === null) {
+      return null;
+    }
+
+    return new UserDto(result);
+  }
 }
