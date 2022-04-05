55b4eae9b4d83a657b294d21704c0e80076d2dfa
itedya <kontakt@itedya.com>
2022-04-05 20:23:09 +0200

    Created settings module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
 import { BotModule } from './bot/bot.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule, BotModule],
+  imports: [AuthModule, PrismaModule, ConfigModule, BotModule, SettingsModule],
 })
 export class AppModule {}
